8e54cca903f4a41b74467ccc80f5a527a4b9c8dd
ManuelRuizJ <195116@iberopuebla.mx>
2026-09-08 11:21:31 -0600

    add /health endpoint

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def inicio():
-    return '¡Hola, Mundo! Este es mi servidor base en Flask.'
+@app.route('/health')
+def health():
+    return jsonify({"status": 'OK',
+                    "entorno": 'Listo'} )
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
